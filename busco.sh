0f4a7bb409238a5bfcbb82ded449075a3cc80289
aanakamo <aanakamo@ucsc.edu>
2023-09-15 10:25:40 -0700

    busco

--- busco.sh
+++ busco.sh
@@ -13,6 +13,19 @@
 
 cd /hb/home/aanakamo/bootcamp2023
 module load busco
+mkdir -p BUSCO
+cd BUSCO
+
+genome_name=$1
+lineage=$2
 
 ### run busco on the Wolbachia assemblies
+busco -i ../FNA/${genome_name}.fna -o ${genome_name} -l ${lineage} -m genome -c ${SLURM_NTASKS} -f --download_path ${genome_name}
 
+## to start individual jobs for each genome using this script, run:
+# cd ~
+# lineage="rickettsiales_odb10"
+# cat bootcamp2023/Wolbachia_genome_tbl.txt | while read line; do
+#     name=$(echo ${line} | awk '{ print $1 }')
+#     sbatch bmeb_boot/busco.sh ${name} ${lineage}
+# done
