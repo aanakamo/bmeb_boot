4032f21bf83b8eec65fabcddec57301addbd493d
aanakamo <aanakamo@ucsc.edu>
2023-09-15 13:33:39 -0700

    orthofinder

--- orthofinder_conda.sh
+++ orthofinder_conda.sh
@@ -7,7 +7,6 @@
 #SBATCH --mail-user=aanakamo@ucsc.edu   # Where to send mail
 #SBATCH --ntasks=1                      # Number of tasks to run
 #SBATCH --cpus-per-task=2               # Number of CPU cores to use per task
-#SBATCH --mem=4G                        # Ammount of RAM to allocate for the task
 #SBATCH --output=slurm_%j.out             # Standard output and error log
 #SBATCH --error=slurm_%j.err              # Standard output and error log
 
