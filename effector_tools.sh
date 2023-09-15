0573e15aa017ec3abe00dbba223573b5430f843f
aanakamo <aanakamo@ucsc.edu>
2023-09-15 11:35:38 -0700

    effector tools install

---
+++ effector_tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --partition=Instruction         # Partition/queue to run on
+#SBATCH --time=08:00:00                 # Max time for job to run (4 hours)
+#SBATCH --job-name=orthofinder_install            # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                 # Mail events(NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=aanakamo@ucsc.edu   # Where to send mail
+#SBATCH --ntasks=1                      # Number of tasks to run
+#SBATCH --cpus-per-task=2               # Number of CPU cores to use per task
+#SBATCH --mem=4G                        # Ammount of RAM to allocate for the task
+#SBATCH --output=slurm_%j.out             # Standard output and error log
+#SBATCH --error=slurm_%j.err              # Standard output and error log
+
+cd /hb/home/aanakamo/bootcamp2023/EFFECTORS
+
+wget https://services.healthtech.dtu.dk/cgi-bin/sw_request?software=signalp&version=5.0&packageversion=5.0b&platform=Linux
+tar -xvzf *tar.gz
