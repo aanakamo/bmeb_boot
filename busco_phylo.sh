ef7b6aaa339de900daa040eacaa7d55a2a3f6239
aanakamo <aanakamo@ucsc.edu>
2023-09-16 14:06:00 -0700

    busco phylo

---
+++ busco_phylo.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+#SBATCH --partition=Instruction         # Partition/queue to run on
+#SBATCH --time=08:00:00                 # Max time for job to run (4 hours)
+#SBATCH --job-name=buscophylo_install            # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                 # Mail events(NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=aanakamo@ucsc.edu   # Where to send mail
+#SBATCH --ntasks=1                      # Number of tasks to run
+#SBATCH --cpus-per-task=2               # Number of CPU cores to use per task
+#SBATCH --output=slurm_%j.out             # Standard output and error log
+#SBATCH --error=slurm_%j.err              # Standard output and error log
+
+cd /hb/home/aanakamo/bootcamp2023
+
+git clone https://github.com/jamiemcg/BUSCO_phylogenomics
+cd BUSCO_phylogenomics
+
+conda env create -f conda_env.yaml
+#conda activate BUSCO_phylogenomics
